815b0e1b485c77c51a40762ab9a20b052f81f271
debasishdasagt <debasishdasagt@users.noreply.github.com>
2021-03-02 08:57:12 +0530

    ccc

--- index.php
+++ index.php
@@ -4,10 +4,13 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php
+include_once 'config.php';
+?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>MLM</title>
     </head>
     <body>
         <?php
