4e79cae4cb8e7be8e05e0c3a6c7ad89fe442bf3c
debasishdasagt <debasishdasagt@users.noreply.github.com>
2021-03-04 08:12:35 +0530

    1st

--- index.php
+++ index.php
@@ -11,10 +11,18 @@ include_once 'config.php';
     <head>
         <meta charset="UTF-8">
         <title>MLM</title>
+        <link rel="stylesheet" href="bootstrap/bootstrap-4.6.0-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="css/style.css">
+        
+        <script type="text/javascript" src="bootstrap/bootstrap-4.6.0-dist/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="d-flex justify-content-center" style="margin-top: 200px">
+            <div class="flex-row">
+                <div class="flex- col-lg-6 col-md-12 col-sm-12 loginBox">
+                    
+                </div>
+            </div>
+        </div>
     </body>
 </html>
