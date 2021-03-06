8b7eb2f8845b5f55eec463357c1f47903398fca2
debasishdasagt <debasishdasagt@users.noreply.github.com>
2021-03-06 08:13:40 +0530

    gffdg

--- index.php
+++ index.php
@@ -11,17 +11,24 @@ include_once 'config.php';
     <head>
         <meta charset="UTF-8">
         <title>MLM</title>
-        <link rel="stylesheet" href="bootstrap/bootstrap-4.6.0-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="bootstrap/bootstrap-3.4.1-dist/css/bootstrap.css">
         <link rel="stylesheet" href="css/style.css">
         
-        <script type="text/javascript" src="bootstrap/bootstrap-4.6.0-dist/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="bootstrap/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
     </head>
     <body>
-        <div class="d-flex justify-content-center" style="margin-top: 200px">
-            <div class="flex-row">
-                <div class="flex- col-lg-6 col-md-12 col-sm-12 loginBox">
-                    
+        <div class="container" style="margin-top: 200px">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 hidden-sm hidden-xs"></div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 loginBox">
+                    <form name="login" action="login.php" method="post">
+                        <input type="text" class="form-control" name="usrid" id="usrid">
+                        <input type="password" class="form-control" name="usrpass" id="usrpass">
+                        <input type="submit" value="Login" class="btn-info btn-sm">
+                    </form>
                 </div>
+                <div class="col-lg-3 col-md-3 hidden-sm hidden-xs"></div>
             </div>
         </div>
     </body>
