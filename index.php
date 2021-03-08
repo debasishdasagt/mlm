363e086d9d3e68538d64bb44d5d4e7399438f3b6
debasishdasagt <debasishdasagt@users.noreply.github.com>
2021-03-08 08:31:17 +0530

    loginUI_Complete

--- index.php
+++ index.php
@@ -11,22 +11,35 @@ include_once 'config.php';
     <head>
         <meta charset="UTF-8">
         <title>MLM</title>
+        
+        
+        <script src="js/jquery-3.6.0.min.js"></script>
+        <script type="text/javascript" src="bootstrap/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
+        
         <link rel="stylesheet" href="bootstrap/bootstrap-3.4.1-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="bootstrap/bootstrap-icons-1.4.0/bootstrap-icons.css">
         <link rel="stylesheet" href="css/style.css">
         
-        <script type="text/javascript" src="bootstrap/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
-        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+        
     </head>
-    <body>
+    <body class="bground">
         <div class="container" style="margin-top: 200px">
             <div class="row">
                 <div class="col-lg-3 col-md-3 hidden-sm hidden-xs"></div>
                 <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 loginBox">
-                    <div style="color: blue; font-size:20px"><i class="bi-person-circle">fghfgh</i></div>
+                    <i class="bi-person-circle" style="font-size: 100px; color: aquamarine;"></i>
                     <form name="login" action="login.php" method="post">
-                        <input type="text" class="form-control" name="usrid" id="usrid">
-                        <input type="password" class="form-control" name="usrpass" id="usrpass">
-                        <input type="submit" value="Login" class="btn-info btn-sm">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="bi-person"></i></span>
+                            <input type="text" class="form-control" name="usrid" id="usrid" placeholder="Username">
+                        </div>
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="bi-lock"></i></span>
+                            <input type="password" class="form-control" name="usrpass" id="usrpass" placeholder="Password">
+                        </div>
+                        
+                        <input type="submit" value="Login" class="btn btn-info btn-sm">
+                        
                     </form>
                 </div>
                 <div class="col-lg-3 col-md-3 hidden-sm hidden-xs"></div>
